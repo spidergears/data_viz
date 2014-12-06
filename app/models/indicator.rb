9e6515a61b31c55ad3d2372021fadb6b387088b4
Siva Gollapalli <siva@joshsoftware.com>
2014-12-06 19:30:47 +0530

    Added indicator model

---
+++ app/models/indicator.rb
@@ -0,0 +1,8 @@
+class Indicator
+  include Mongoid::Document
+
+  field :code
+  field :name
+  field :description
+  field :detail_description
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+file = CSV.open("#{Rails.root}/db/Health.csv", 'r')
+columns = file.readline
+
+while(row = file.readline) do 
+  p row
+  Indicator.create!(code: row[0], name: row[1], description: row[3], detail_description: row[5])
+end
