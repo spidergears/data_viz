9d45db77f6f26d282ef232d92723f9c051aa7fa5
Siva Gollapalli <siva@joshsoftware.com>
2014-12-06 20:56:11 +0530

    Modified models relations and update db seed

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -1,8 +1,11 @@
 class Indicator
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :code
   field :name
   field :description
   field :detail_description
+
+  has_many :statistics
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,26 @@ require 'csv'
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 
-file = CSV.open("#{Rails.root}/db/Health.csv", 'r')
-columns = file.readline
+#file = CSV.open("#{Rails.root}/db/Health.csv", 'r')
+#columns = file.readline
+
+#while(row = file.readline) do 
+#  Indicator.create!(code: row[0], name: row[1], description: row[3], detail_description: row[5])
+#end
+
+table = CSV.table("#{Rails.root}/db/dummy.csv")
+header = table.headers
+
+file = CSV.open("#{Rails.root}/db/dummy.csv", 'r', headers: true)
 
 while(row = file.readline) do 
-  p row
-  Indicator.create!(code: row[0], name: row[1], description: row[3], detail_description: row[5])
+  p row[3]
+  indicator = Indicator.where(code: row[3]).first
+  p indicator
+
+  data = WdiData.create!(country: row[0], country_code: row[1], indicator: indicator)
+
+  header[4..-1].each do |year|
+    Statistic.create!(indicator: indicator, wdi_data: data, value: row[year.to_s], country: row[0])
+  end
 end
